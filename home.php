65f30baa88a17546271307576d45a27d0735f49c
Markus Tammeoja <Uptaker@users.noreply.github.com>
2020-09-08 13:36:48 +0300

    kodutöö, semestri kalkulatsioonid

--- home.php
+++ home.php
@@ -15,7 +15,26 @@
 	$semesterduration = $semesterstart->diff($semesterend);
 	$semesterdurationdays = $semesterduration->format("%r%a");
 	$today = new DateTime("now");
+	$semesterleft = $semesterstart->diff($today);
+	$semesterleftdays = $semesterleft->format("%r%a");
+	//percentages below
+	$percentageelapsed = $semesterleftdays/$semesterdurationdays*100;
+	$percentageleft = 100-$percentageelapsed;
+
+
 	//KODUNETÖÖ https://github.com/Veebiprogrammeerimine-2020/ryhm-1
+	if($today = $semesterduration) { //õppetöö staatus
+		$semesterstatus = "1. semestri õppetöö on aktiivne ning on möödunud " .$semesterleftdays 
+		." päeva. Läbi on " .$percentageelapsed ." protsenti. Jäänud on " .$percentageleft ." protsenti";
+	}
+	if($today > $semesterduration) {
+		$semesterstatus = "1. semestri õppetöö on läbi";
+	}
+	if($today < $semesterduration) {
+		$semesterstatus = "1. semestri õppetöö ei ole veel alanud";
+	}
+	//õppetöö protsent
+
 
 ?>
 <!DOCTYPE html>
@@ -25,10 +44,12 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
-		<h3>Võite vaadata vabalt ringi</h3><br>
+		<h1>Võite vaadata vabalt ringi</h1><br>
 		<p>Ring on <a href="https://areait.com.au/wp-content/uploads/2017/12/circle-png-circle-icon-1600.png">siin</a>!</p>
 		<p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
-		<p><?php echo "Praegu on " .$partofday ."."; ?></p>
+		<p><?php echo "Kellaajaliselt, praegu oleks " .$partofday ."."; ?></p>
 		<p><?php echo "Veebilehe looja on " .$username ."." ?><p>
+		<h3><?php echo $semesterstatus; ?>.<h3>
+		<p><?php $semesterdurationday; ?></p>
 	</body>
 </html>
