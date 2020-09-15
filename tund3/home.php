165962deb46fbc88b38e1245f026f8d229182050
Markus Tammeoja <Uptaker@users.noreply.github.com>
2020-09-15 09:50:56 +0300

    kolmas tund, esimene pool

--- tund3/home.php
+++ tund3/home.php
@@ -9,6 +9,11 @@
 	if($hournow >= 8 and $hournow <= 18) {
 		$partofday = "õppimise aeg";
 	}
+  
+	$weekdayNameset = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+	$monthNameset = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+	$weekdaynow = date("N");
+
 	#vaatame semestri kulgemist
 	$semesterstart = new DateTime("2020-8-31");
 	$semesterend = new DateTime("2020-12-13");
@@ -23,19 +28,41 @@
 
 
 	//KODUNETÖÖ https://github.com/Veebiprogrammeerimine-2020/ryhm-1
-	if($today = $semesterduration) { //õppetöö staatus
+	if($today = $semesterduration) { //õppetöö aktiivne
 		$semesterstatus = "1. semestri õppetöö on aktiivne ning on möödunud " .$semesterleftdays 
 		." päeva. Läbi on " .$percentageelapsed ." protsenti. Jäänud on " .$percentageleft ." protsenti";
 	}
-	if($today > $semesterduration) {
-		$semesterstatus = "1. semestri õppetöö on läbi";
+	if($today > $semesterduration) { // läbi
+		$semesterstatus = "1. semestri õppetöö on läbi. Läbi on 100%.";
 	}
-	if($today < $semesterduration) {
-		$semesterstatus = "1. semestri õppetöö ei ole veel alanud";
+	if($today < $semesterduration) { //pole alanud
+		$semesterstatus = "1. semestri õppetöö ei ole veel alanud. Läbi on 0%.";
 	}
 	//õppetöö protsent
+	
+	//annan ette lubatud pildivormingute loendi
+	$picfiletypes = ["image/jpeg", "image/png"];
+	//loeme piltide kataloogi sisu ja näitame pilte
+	//$allfiles = scandir("../vp_pics/");
+	$allfiles = array_slice(scandir("../vp_pics/"), 2);
+	// var_dump($allfiles);
+	//$picfiles = array_slice($allfiles, 2);
+	$picfiles = [];
+	foreach($allfiles as $thing) {
+		$fileinfo = getImagesize("../vp_pics/" .$thing);
+		if(in_array($fileinfo["mime"], $picfiletypes) == true) {
+			array_push($picfiles, $thing);
+		}
+	}
 
-
+	//paneme kõik pildid ekraanile
+	$piccount = count($picfiles);
+	//$i + 1;
+	//$i ++
+	$imghtml = "";
+	for($i = 0; $i < $piccount; $i ++) {
+		$imghtml .= '<img src="../vp_pics/' .$picfiles[$i] .'" alt="Tallinna Ülikool">';
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,12 +71,14 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
+		<img src="../img/vp_banner.png" alt="Veebiprogrammeerimise pilt">
 		<h1>Võite vaadata vabalt ringi</h1><br>
 		<p>Ring on <a href="https://areait.com.au/wp-content/uploads/2017/12/circle-png-circle-icon-1600.png">siin</a>!</p>
-		<p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
+		<p>Lehe avamise hetk: <?php echo $weekdayNameset[$weekdaynow-1] .", " .$fulltimenow; ?>.</p>
 		<p><?php echo "Kellaajaliselt, praegu oleks " .$partofday ."."; ?></p>
 		<p><?php echo "Veebilehe looja on " .$username ."." ?><p>
 		<h3><?php echo $semesterstatus; ?>.<h3>
 		<p><?php $semesterdurationday; ?></p>
+		<p><?php echo $imghtml; ?></p>
 	</body>
 </html>
